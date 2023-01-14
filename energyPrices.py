b8f032ac7abdaab5151b05d988ca9365dccd034a
kinghos <kingshukhossain@outlook.com>
2023-01-14 19:40:01 +0000

    Removed unnecessary request session

--- energyPrices.py
+++ energyPrices.py
@@ -1,14 +1,13 @@
 import requests
 import json
 
-START_DATE = "13-01-2023"
-END_DATE = "14-01-2023"
+START_DATE = "12-01-2023"
+END_DATE = "13-01-2023"
 DNO = 12 # Regional code - defaults to 12 (London)
 VOLTAGE = "LV"
 api_url = f"https://odegdcpnma.execute-api.eu-west-2.amazonaws.com/development/prices?dno={DNO}&voltage={VOLTAGE}&start={START_DATE}&end={START_DATE}"
 
-with requests.Session() as s: # Starts a request session
-    response = s.get(api_url).text # Gets the json from the url
+response = requests.get(api_url).text # Gets the json from the url
 
 response_info = json.loads(response) # Loads json into a dictionary
 print(response_info)
