7594afd20b99caf04cdcbdee2d82ed331a433d32
kinghos <kingshukhossain@outlook.com>
2023-01-14 19:38:24 +0000

    Re-adjusted to work with new API and deleted old save

--- energyPrices.py
+++ energyPrices.py
@@ -1,32 +1,16 @@
-import datetime
 import requests
-import csv
-import pandas as pd
-
-def settlementPeriod(time):
-    # now = datetime.datetime.now()
-    hours = time.strftime("%H")
-    mins = time.strftime("%M")
-    hours, mins = int(hours), int(mins)
-    mins += hours * 60
-    return mins // 30 + 1 # Settlement periods calculated
+import json
 
 START_DATE = "13-01-2023"
 END_DATE = "14-01-2023"
-DNO = 12
+DNO = 12 # Regional code - defaults to 12 (London)
 VOLTAGE = "LV"
-api_url = f"https://odegdcpnma.execute-api.eu-west-2.amazonaws.com/development/prices?{DNO}&{VOLTAGE}&P{START_DATE}&{END_DATE}"
+api_url = f"https://odegdcpnma.execute-api.eu-west-2.amazonaws.com/development/prices?dno={DNO}&voltage={VOLTAGE}&start={START_DATE}&end={START_DATE}"
 
 with requests.Session() as s: # Starts a request session
-    download = s.get(api_url) # Gets the CSV from the url
+    response = s.get(api_url).text # Gets the json from the url
 
-    decoded_content = download.content.decode('utf-8') # Decodes the CSV into utf-8 string format
-
-    csvReader = csv.reader(decoded_content.splitlines(), delimiter=',') # Reads the CSV with a reader object
-    my_list = list(csvReader) # Object is converted to list
-    
-    with open(r"C:\Users\user\Documents\Homework\Young Engineers\API\data.csv", 'w') as file:
-        writer = csv.writer(file) # Writer object created
-        writer.writerows(my_list) # Writer object writes to file
-    
-    
+response_info = json.loads(response) # Loads json into a dictionary
+print(response_info)
+with open("data.json", "w") as f:
+    json.dump(response_info, f, indent=4) # Dumps json file 
