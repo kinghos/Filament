50df6cf3168c7abc4ef8c18ac64a47aa99f5ad0c
kinghos <kingshukhossain@outlook.com>
2023-01-15 14:12:18 +0000

    Finished emissions API get

--- emissionsCalc.py
+++ emissionsCalc.py
@@ -7,7 +7,11 @@ headers = {
 }
  
 response = requests.get('https://api.carbonintensity.org.uk/intensity', params={}, headers = headers)
+emissionsDict = response.json()
 
 with open(r"C:\Users\user\Documents\Homework\Young Engineers\API\emissions.json", "w") as f: # File is here for development purposes; not actually needed
     f.seek(0) # Allows overwriting
-    json.dump(response, f, indent=4) # Dumps json file
+    json.dump(emissionsDict, f, indent=4) # Dumps json file
+
+val = emissionsDict["data"][0]["intensity"]["actual"]
+print(f"{val}gCO2/kWh")
