9f0006707cfb4fda58800a0afbd3aeb2f33d34da
kinghos <kingshukhossain@outlook.com>
2023-01-22 18:22:19 +0000

    Sunrise/sunset API call complete

--- sunTimes.py
+++ sunTimes.py
@@ -5,8 +5,14 @@ LATITUDE = 0.0
 LONGITUDE = 0.0
 
 def getSunTimes():
-    reponse = requests.get(f"https://api.sunrise-sunset.org/json?lat={LATITUDE}&lng={LONGITUDE}")
-    with open(r"C:\Users\user\Documents\Homework\Young Engineers\API\suntimes.json", "w") as f: # File is here for development purposes; not actually needed
+    response = requests.get(f"https://api.sunrise-sunset.org/json?lat={LATITUDE}&lng={LONGITUDE}")
+    response = response.json()
+
+    with open("sunTimes.json", "w") as f: # File is here for development purposes; not actually needed
         f.seek(0) # Allows overwriting
         json.dump(response, f, indent=4) # Dumps json file
     
+    return response["results"]["sunrise"], response["results"]["sunset"]
+
+sunrise, sunset = getSunTimes()   
+print(f"Sunrise: {sunrise}\nSunset: {sunset}")
