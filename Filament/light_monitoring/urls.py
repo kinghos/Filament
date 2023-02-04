12da44c2fe0b0c2c035bfc1428ab78938cfc9d0a
kinghos <kingshukhossain@outlook.com>
2023-02-04 19:33:36 +0000

    fix

--- Filament/light_monitoring/urls.py
+++ Filament/light_monitoring/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from .views import *
+from light_monitoring.views import SettingsView
 
 urlpatterns = [
     path('', filament, name='filament'),
     path('data/', data, name="data"),
-    path('settings/', settings, name="settings"),
+    path('settings/', SettingsView.as_view(), name="settings"),
 ]
